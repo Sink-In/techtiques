48d5336d24d712589ee71f83657518ebd76231f1
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2018-03-11 21:18:20 -0400

    Factories created

--- database/factories/NewFactory.php
+++ database/factories/NewFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\News::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->name,
+        'slug' => str_slug($faker->name, '-'),
+        'description' => $faker->text(20),
+        'image' => str_slug($faker->name, '-') . '.jpg',
+        'source' => str_slug($faker->name, '-'),
+        'url' => str_slug($faker->name, '-')
     ];
 });
--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Topic::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->name,
+        'slug' => str_slug($faker->name, '-'),
+        'description' => $faker->text(20),
+        'image' => str_slug($faker->name, '-') . '.jpg',
     ];
 });
